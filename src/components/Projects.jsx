f08d5c60d25f93cd85a75d5eb7187b642e5bff85
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-03-30 14:02:34 +0530

    Add files via upload

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
             </div>
 
             <div className='flex'>
-            <a href="https://github.com/atharvpatwardhan/StockMarketPrediction" target="_blank"><img src = {stock} alt="stock market prediction" className='hover:scale-110 hover:bg-gradient-to-r from-red-600 to-orange-500' style={{"marginLeft":"80%","marginTop":"40%","height":"250px","borderRadius":"20%"}}/></a>
+            <a href="https://github.com/atharvpatwardhan/StockMarketPrediction"><img src = {stock} alt="stock market prediction" className='hover:scale-110 hover:bg-gradient-to-r from-red-600 to-orange-500' style={{"marginLeft":"80%","marginTop":"40%","height":"250px","borderRadius":"20%"}}/></a>
             <a href="https://github.com/atharvpatwardhan/Employee-Information-System-Using-SQL"><img src = {eis} alt="Employee Information System" className='hover:scale-110 hover:bg-gradient-to-r from-red-600 to-orange-500' style={{"marginLeft":"130%","marginTop":"40%","height":"250px","borderRadius":"20%"}}/></a>
             </div>
         </div>
