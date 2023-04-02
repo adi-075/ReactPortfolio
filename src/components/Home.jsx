97cc2d1bcacb6231b826a5e83d208d456c224abf
adi-075 <adityamhambrey323@gmail.com>
2023-04-02 09:45:37 +0530

    Add typewriter to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,33 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-    return(
-        <div style={{'height':"750px"}} id="home">
-            <div className="text-center" style={{"marginTop":"13%","marginLeft":"25%","position":"absolute"}}>
-            <h1 className="text-9xl text-transparent bg-gradient-to-r from-purple-500 to-blue-600 bg-clip-text">Hello World,</h1>
-            <h1 className="my-3 text-9xl text-transparent bg-gradient-to-r from-purple-500 to-blue-600 bg-clip-text">It's Atharv!</h1>
-            <h3 className="text-center mx-4 my-9 text-7xl text-transparent bg-gradient-to-r from-red-600 to-orange-500 bg-clip-text">I'm a  software developer.</h3>
-
-            </div>
-
-        </div>
-
-    )
+  return (
+    <div style={{ 'height': "750px" }} id="home" className='max-w-screen w-full h-full bg-gradient-to-r from-gray-900 via-gray-900 to-gray-900'>
+      <div className="text-center">
+        <h1 className="font-normal text-6xl next-9xl text-transparent text-white">
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className='typewriter'>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Hello, World!")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("My name is Atharv Patwardhan.")
+                  .pauseFor(1000)
+                  .start();
+              }}
+            />
+          </div>
+        </h1>
+        <h3 className="text-center py-3 text-6xl text-transparent bg-gradient-to-r from-blue-600 to-green-500 bg-clip-text">I'm a software developer.</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
