63a16e922cf67ebdd6e0794ce1869e283aa950dd
adi-075 <adityamhambrey323@gmail.com>
2023-04-02 09:45:43 +0530

    Update App

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
-    <div className='bg-gradient-to-b from-slate-900 to-slate-700'>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
